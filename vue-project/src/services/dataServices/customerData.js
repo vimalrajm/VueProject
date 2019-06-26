dc9cc6d5d8dafc0655d5b597e8825488b28887af
vimalrajm <vimalraj.nie@gmail.com>
2019-06-27 01:39:44 +0530

    changes made

--- vue-project/src/services/dataServices/customerData.js
+++ vue-project/src/services/dataServices/customerData.js
@@ -1,5 +1,5 @@
 export default class CustomerData {
-  constructor(id, name, email, country, orders, role, password, address) {
+  constructor(id, name, email, country, orders, role, password, address, zipcode, city) {
     this.id = id;
     this.name = name;
     this.email = email;
@@ -8,5 +8,7 @@ export default class CustomerData {
     this.role = role;
     this.password = password;
     this.address = address;
+    this.zipcode = zipcode;
+    this.city = city;
   }
 }
